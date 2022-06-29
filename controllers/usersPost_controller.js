63bd35e9c5345a6c6a6cf9a1f0d4f3f6330b81d5
piyush-1871 <piyush1871b@gmail.com>
2022-06-29 22:35:56 +0530

    set up view engine and home ejs

--- controllers/usersPost_controller.js
+++ controllers/usersPost_controller.js
@@ -1,3 +1,7 @@
 module.exports.post = function(req,res){
-    return res.end('<h1>Users Post</h1>');
+    // return res.end('<h1>Users Post</h1>');
+
+    return res.render('posts',{
+        title : "Posts"
+    })
 }
